a2dc2c4bb36be327e9d39e7bd57e7b91a1cafcd7
Oriol Bover Vila <boversauros@Oriols-MacBook-Pro.local>
2019-03-31 14:07:07 +0200

    add styles

--- drumKit/script.js
+++ drumKit/script.js
@@ -1,10 +1,21 @@
 'use strict'
 
+const removeStyle = event => {
+    if (event.propertyName !== 'transform') return
+    event.target.classList.remove('playing')
+}
+
 const drumer = event => {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    const key = document.querySelector(`div[data-key="${event.keyCode}"]`)
     if (!audio) return
+
+    key.classList.add('playing')
     audio.currentTime = 0
     audio.play()
 }
 
+const drumKeys = document.querySelectorAll('.drum-key')
+drumKeys.forEach(key => key.addEventListener('transitionend', e => removeStyle(e)))
 window.addEventListener('keydown', e => drumer(e))
+
