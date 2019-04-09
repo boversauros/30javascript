80a33f24cd251debeb9c185f55853fe728738b2d
Oriol <bover73@gmail.com>
2019-04-09 12:42:16 +0200

    add flexPanels

---
+++ flexPanels/scripts.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const panels = document.querySelectorAll('.panel')
+
+function toggleOpen() {
+    this.classList.toggle('open')
+}
+
+function toggleOpenActive(e) {
+    if (e.propertyName.includes('flex')) this.classList.toggle('open-active')
+}
+
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+panels.forEach(panel => panel.addEventListener('transitionend', toggleOpenActive))
